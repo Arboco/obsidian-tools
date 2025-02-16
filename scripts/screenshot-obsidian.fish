c49156f1f971d59dfa6d3847a6110254f72b2b2a
Anon <codeort@protonmail.com>
2025-02-16 01:21:13 +0100

    put background scripts into their own folder and updated the sccript to look for the scripts inside the proper relative folder

--- scripts/screenshot-obsidian.fish
+++ scripts/screenshot-obsidian.fish
@@ -4,6 +4,7 @@ set filename "$argv[1]"
 set note_file (find $obsidian -type f -name "$argv[1].md" -not -path '*/[@.]*' -type f -mtime -2)
 set folder_title (echo $argv[1] | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
 set screenshot_folder "$obsidian/resources/game/screenshots/$folder_title"
+set script_dir (realpath (status dirname))
 
 if test -d $screenshot_folder
   echo "folder exists"
@@ -35,13 +36,14 @@ evtest $devinput | while read line
     echo -e "![[$fv_name]]\n" >> "$note_file"
 
     # required because ffmpeg is buggy as a background process so this serves as a watcher to escape screen capture on demand 
-    ffmpeg-escaper.fish &
+    $script_dir/ffmpeg-escaper.fish &
 
     # required to grab active window data so that in windowed mode only the game is captured 
     for line in (xdotool getactivewindow getwindowgeometry --shell)
       set -gx (echo $line | cut -d= -f1) (echo $line | cut -d= -f2)
     end
 
+    # first three lines are to be able to use two audice devices (headphones and speakers) and be able to capture audio no matter what, though don't switch audio device mid recording
     ffmpeg \
     -thread_queue_size 1024 -f pulse -i alsa_output.usb-FiiO_DigiHug_USB_Audio-01.analog-stereo.monitor \
     -thread_queue_size 1024 -f pulse -i alsa_output.pci-0000_0d_00.4.analog-stereo.monitor \
