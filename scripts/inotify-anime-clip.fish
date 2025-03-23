97587baffc33749adac1c05b290773ea18b5d13a
Anon <codeort@protonmail.com>
2025-03-24 00:14:12 +0100

    changed so everything gets converted for better universal compatibility

--- scripts/inotify-anime-clip.fish
+++ scripts/inotify-anime-clip.fish
@@ -22,14 +22,13 @@ inotifywait -m -e create --format '%w%f' $a_path | while read FILE
   set timestamp (date +%s)
 
   # x265 hevc codec doesn't work on my obsidian so I convert it into a friendlier codec 
-  if test $codec = 'hevc' 
-    echo "hevc codec detected, conversion started."
-    ffmpeg -i $FILE -c:v libx264 -crf 18 -preset slow -c:a aac -b:a 320k $screenshot_folder/$folder_title$timestamp.mp4
-    echo -e "![[$folder_title$timestamp.mp4]]\n" >> $obsidian_md
-    rm $FILE
-  else
-    echo "File is compatible, going to move it as is."
-    mv $FILE "$screenshot_folder/$folder_title$timestamp.$file_end"
-    echo -e "![[$folder_title$timestamp.$file_end]]\n" >> $obsidian_md
-  end
+  #if test $codec = 'hevc' 
+  #echo "hevc codec detected, conversion started."
+  ffmpeg -i $FILE -c:v libx264 -crf 18 -preset slow -c:a aac -b:a 320k $screenshot_folder/$folder_title$timestamp.mp4
+  echo -e "![[$folder_title$timestamp.mp4]]\n" >> $obsidian_md
+  rm $FILE
+  
+  #echo "File is compatible, going to move it as is."
+  #mv $FILE "$screenshot_folder/$folder_title$timestamp.$file_end"
+  #echo -e "![[$folder_title$timestamp.$file_end]]\n" >> $obsidian_md
 end
