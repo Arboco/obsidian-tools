dc7d7a4e8491255118d9a65a3e8dd8e6d8d1b715
Anon <codeort@protonmail.com>
2025-03-28 01:15:58 +0100

    changed name of mindpalace tool

--- scripts/inotify-anime-clip.fish
+++ scripts/inotify-anime-clip.fish
@@ -7,28 +7,26 @@ set a_path (echo $a_path | grep -oP '(?<=path: ).*$')
 set folder_title (echo $title | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
 set screenshot_folder "/home/anon/ortup/important/notes/ortvault/resources/anime_db/media/clips/$folder_title"
 
-
 if test -d $screenshot_folder
-  echo "folder exists"
+    echo "folder exists"
 else
-  mkdir $screenshot_folder
+    mkdir $screenshot_folder
 end
 
-
 inotifywait -m -e create --format '%w%f' $a_path | while read FILE
-  sleep 3
-  set codec (ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of csv=p=0 $FILE)
-  set file_end (echo "$(basename $FILE)" | grep -o '...$')
-  set timestamp (date +%s)
+    sleep 3
+    set codec (ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of csv=p=0 $FILE)
+    set file_end (echo "$(basename $FILE)" | grep -o '...$')
+    set timestamp (date +%s)
 
-  # x265 hevc codec doesn't work on my obsidian so I convert it into a friendlier codec 
-  #if test $codec = 'hevc' 
-  #echo "hevc codec detected, conversion started."
-  ffmpeg -i $FILE -c:v libx264 -crf 18 -preset slow -c:a aac -b:a 320k $screenshot_folder/$folder_title$timestamp.mp4
-  echo -e "![[$folder_title$timestamp.mp4]]\n" >> $obsidian_md
-  rm $FILE
-  
-  #echo "File is compatible, going to move it as is."
-  #mv $FILE "$screenshot_folder/$folder_title$timestamp.$file_end"
-  #echo -e "![[$folder_title$timestamp.$file_end]]\n" >> $obsidian_md
+    # x265 hevc codec doesn't work on my obsidian so I convert it into a friendlier codec 
+    #if test $codec = 'hevc' 
+    #echo "hevc codec detected, conversion started."
+    ffmpeg -i $FILE -c:v libx264 -crf 18 -preset slow -c:a aac -b:a 320k $screenshot_folder/$folder_title$timestamp.mp4
+    echo -e "![[$folder_title$timestamp.mp4]]\n" >>$obsidian_md
+    rm $FILE
+
+    #echo "File is compatible, going to move it as is."
+    #mv $FILE "$screenshot_folder/$folder_title$timestamp.$file_end"
+    #echo -e "![[$folder_title$timestamp.$file_end]]\n" >> $obsidian_md
 end
