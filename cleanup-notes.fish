47183377fd1ff308c2466d1f2ad64d3b51f207c7
Anon <codeort@protonmail.com>
2025-04-10 15:08:51 +0200

    added small script to clean up note resources no longer used

---
+++ cleanup-notes.fish
@@ -0,0 +1,16 @@
+#! /usr/bin/env fish
+
+for i in $(find /home/anon/ortup/important/notes/ortvault/resources/* -type f)
+    set filename (basename $i)
+    if grep -r "\[\[$filename\]\]" /home/anon/ortup/important/notes/ortvault/notes/
+    else
+        mv $i /home/anon/ortup/important/notes/ortvault/restrash
+    end
+end
+
+for i in $(find /home/anon/ortup/important/notes/ortvault/restrash/* -type f)
+    set filename (basename $i)
+    if grep -r "/$filename" /home/anon/ortup/important/notes/ortvault/notes/canvas/
+        mv $i /home/anon/ortup/important/notes/ortvault/resources/canvas/
+    end
+end
