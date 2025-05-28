a6903b19efc693da10810f12954ee780dc6e4fc0
Anon <codeort@protonmail.com>
2025-05-28 17:50:55 +0200

    small fix for sort-notes

--- sort-notes.fish
+++ sort-notes.fish
@@ -12,8 +12,9 @@ for i in (find /home/anon/ortup/important/notes/ortvault/ /home/anon/ortup/impor
             set raw_grep (grep "  - $inner" $i; or  grep -oP "(?<=tags: )$inner" $i >/dev/null 2>&1)
             set refined_grep (echo $raw_grep | grep -o "[a-zA-Z]*")
             set target_folder (grep -oP "(?<=$refined_grep:).*" $sort_yaml)
-            mkdir -p $obsidian_main/$notes_folder/$target_folder
-            mv $i $obsidian_main/$notes_folder/$target_folder
+            set target_folder (string trim "$target_folder")
+            mkdir -p "$obsidian_main/$notes_folder/$target_folder"
+            mv $i "$obsidian_main/$notes_folder/$target_folder"
             set filename (basename $i)
             echo -e (set_color red)"Moved"(set_color normal) $filename (set_color red)"to"(set_color normal) $obsidian_main/$notes_folder/$target_folder
             break
