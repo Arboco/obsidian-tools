6c624e4c519302f424f4bfab68b22143a39bc5b3
Anon <codeort@protonmail.com>
2025-06-23 13:28:49 +0200

    Lots of polish and refactoring to anime-starter. Added condition so that a certain percentage of the video has to be watched for it to count, else you can continue watching from where you left

--- scripts/inotify-killer.fish
+++ scripts/inotify-killer.fish
@@ -1,6 +1,7 @@
 #! /usr/bin/env fish
-while kill -0 $argv[1] > /dev/null 2>&1
-    echo "Process is running"
-    sleep 1  
+while kill -0 $argv[1] >/dev/null 2>&1
+    sleep 1
 end
 killall inotifywait
+
+echo \a
