a165fdb466fd850f6b5f4c5445be29eea7332b85
Anon <codeort@protonmail.com>
2025-06-24 02:34:41 +0200

    Added some earphone friendly sounds

--- scripts/inotify-killer.fish
+++ scripts/inotify-killer.fish
@@ -1,7 +1,9 @@
-#! /usr/bin/env fish
+#! /usr/bin/env fish 
+set script_dir (dirname (status --current-filename))
+set parent_dir (dirname $script_dir)
 while kill -0 $argv[1] >/dev/null 2>&1
     sleep 1
 end
 killall inotifywait
 
-echo \a
+ffplay -nodisp -autoexit $parent_dir/helper/amazed-emote-animal-crossing.mp3 >/dev/null 2>&1
