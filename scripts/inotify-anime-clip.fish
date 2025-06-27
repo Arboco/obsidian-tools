aa3fc6419c15534adb3e68698f71249883857ad3
Anon <codeort@protonmail.com>
2025-06-28 01:38:38 +0200

    Created film-starter which is just a gutted version of anime-starter

--- scripts/inotify-anime-clip.fish
+++ scripts/inotify-anime-clip.fish
@@ -1,14 +1,26 @@
 #! /usr/bin/env fish
 
-set obsidian_md "$argv[1]"
-set episode_md "$argv[2]"
-set title "$argv[3]"
+if string match film $argv[1]
+    set obsidian_md "$argv[2]"
+    set episode_md $obsidian_md
+    set title "$argv[3]"
+else
+    set obsidian_md "$argv[1]"
+    set episode_md "$argv[2]"
+    set title "$argv[3]"
+end
 set obsidian (ot_config_grab "ObsidianMainFolder")
 set resources_folder (ot_config_grab "ObsidianResourceFolder")
 set folder_title (echo $title | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
 set anime_folder (ot_config_grab "AnimeFolder")
-set screenshot_folder "$obsidian/$resources_folder/$anime_folder/media/clips/$folder_title"
-set a_path (rg -oP '(?<=animepath: ).*$' $obsidian_md)
+
+if string match film $argv[1]
+    set screenshot_folder "$obsidian/$resources_folder/film/media/clips/$folder_title"
+    set a_path (rg -oP '(?<=filmpath: ).*$' $obsidian_md | rg -o '/.*\/')
+else
+    set screenshot_folder "$obsidian/$resources_folder/$anime_folder/media/clips/$folder_title"
+    set a_path (rg -oP '(?<=animepath: ).*$' $obsidian_md)
+end
 
 mkdir -p $screenshot_folder
 
@@ -21,8 +33,14 @@ inotifywait -m -e create --format '%w%f' "$a_path" | while read FILE
     # x265 hevc codec doesn't work on my obsidian so I convert it into a friendlier codec 
     #if test $codec = 'hevc' 
     #echo "hevc codec detected, conversion started."
-    set jap_lang (ffprobe -v error -select_streams a -show_entries stream=index:stream_tags=language -of csv=p=0 $FILE | grep jpn | cut -d',' -f1)
-    ffmpeg -i $FILE -c:v libx264 -crf 25 -preset slow -c:a aac -b:a 320k -map 0:v -map 0:$jap_lang[1] $screenshot_folder/$folder_title$timestamp.mp4
+
+    if string match film $argv[1]
+        set lang (ffprobe -v error -select_streams a -show_entries stream=index:stream_tags=language -of csv=p=0 $FILE | cut -d',' -f1)
+        ffmpeg -i $FILE -c:v libx264 -crf 25 -preset slow -c:a aac -b:a 320k -map 0:v -map 0:$lang[1] $screenshot_folder/$folder_title$timestamp.mp4
+    else
+        set jap_lang (ffprobe -v error -select_streams a -show_entries stream=index:stream_tags=language -of csv=p=0 $FILE | grep jpn | cut -d',' -f1)
+        ffmpeg -i $FILE -c:v libx264 -crf 25 -preset slow -c:a aac -b:a 320k -map 0:v -map 0:$jap_lang[1] $screenshot_folder/$folder_title$timestamp.mp4
+    end
     echo -e "![[$folder_title$timestamp.mp4]]\n" >>$episode_md
     echo "" >>$episode_md
     rm $FILE
--- scripts/inotify-anime-screenshot.fish
+++ scripts/inotify-anime-screenshot.fish
@@ -6,21 +6,27 @@ set obsidian (ot_config_grab "ObsidianMainFolder")
 set resources_folder (ot_config_grab "ObsidianResourceFolder")
 set folder_title (echo $title | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
 set anime_folder (ot_config_grab "AnimeFolder")
-set screenshot_folder "$obsidian/$resources_folder/$anime_folder/media/screenshots/$folder_title"
+if string match film $argv[3]
+    set screenshot_folder "$obsidian/$resources_folder/film/media/screenshots/$folder_title"
+else
+    set screenshot_folder "$obsidian/$resources_folder/$anime_folder/media/screenshots/$folder_title"
+end
 
 mkdir -p $screenshot_folder
 
 inotifywait -m -e create --format '%w%f' $screenshot_folder | while read FILE
     set timestamp (date +%F_%H%M%S)
     mv $FILE $screenshot_folder/$folder_title$timestamp.jpg
-    if rg "cover-img.*thumb" $obsidian_md
-        sed -i /cover-img:/d $obsidian_md
-        sed -i "/^episode:/a\\cover-img: \"!\[\[$folder_title$timestamp.jpg\]\]\"" $obsidian_md
-    end
-
-    if rg "cover-img:" $obsidian_md
+    if string match film $argv[3]
     else
-        sed -i "/^episode:/a\\cover-img: \"!\[\[$folder_title$timestamp.jpg\]\]\"" $obsidian_md
+        if rg "cover-img.*thumb" $obsidian_md
+            sed -i /cover-img:/d $obsidian_md
+            sed -i "/^episode:/a\\cover-img: \"!\[\[$folder_title$timestamp.jpg\]\]\"" $obsidian_md
+        end
+        if rg "cover-img:" $obsidian_md
+        else
+            sed -i "/^episode:/a\\cover-img: \"!\[\[$folder_title$timestamp.jpg\]\]\"" $obsidian_md
+        end
     end
 
     echo -e "![[$folder_title$timestamp.jpg]]\n" >>$obsidian_md
